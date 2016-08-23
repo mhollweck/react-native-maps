cc09024c8ca6d95457d856b36dc1d32f281a41e7
Maria Hollweck <maria.hollweck@kupferwerk.com>
2016-08-23 13:30:36 +0200

    update

--- ios/AirMaps/AIRMap.h
+++ ios/AirMaps/AIRMap.h
@@ -11,7 +11,7 @@
 #import <UIKit/UIKit.h>
 
 #import "RCTConvert+MapKit.h"
-#import "React/RCTComponent.h"
+#import "RCTComponent.h"
 #import "SMCalloutView.h"
 
 extern const CLLocationDegrees AIRMapDefaultSpan;
